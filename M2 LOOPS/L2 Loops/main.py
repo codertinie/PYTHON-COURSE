55a3585ce60d4f5525e401773392581cd77beee8
codertinie <munyao25@gmail.com>
2026-07-30 22:11:04 +0300

    Python Course Module 2 Content

---
+++ M2 LOOPS/L2 Loops/main.py
@@ -0,0 +1,44 @@
+#=======================================
+#       ACTIVITY 1 
+#=======================================
+#input an integer value 
+n = int(input("Enter the number whose sum you want to find: "))
+sum=0
+
+#Iterates for n+1 times: i=1 to n+1
+for i in range(1, n+1): 
+  sum = sum+i
+  print("\nSum =", sum)
+
+
+#=======================================
+#       ACTIVITY 2 
+#=======================================
+#Input a word or sentence
+string = input("Please enter your own String : ")
+
+string2 = ('')
+#loop for printing in reverse 
+for i in string:
+    string2 = i + string2
+    
+print("\nThe Original String = ", string)
+print("The Reversed String = ", string2)
+
+
+#=======================================
+#       ACTIVITY 3 
+#=======================================
+
+# input number greater than 1
+n = int(input("Enter the value of n: "))
+
+# print the numbers from n to 1
+print ("numbers from {0} to {1} are: ".format(n,1))
+
+# loop to print numbers 
+for i in range(n,0,-1):
+	print (i)
+
+
+
